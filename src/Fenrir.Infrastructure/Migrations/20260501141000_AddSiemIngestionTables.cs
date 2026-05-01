37ae1f48d4ba92195e06c1eef020fe95835d3436
Matthew Palmer <zy538324@ou.ac.uk>
2026-05-01 14:13:39 +0100

    Add EF metadata to SIEM migration

--- src/Fenrir.Infrastructure/Migrations/20260501141000_AddSiemIngestionTables.cs
+++ src/Fenrir.Infrastructure/Migrations/20260501141000_AddSiemIngestionTables.cs
@@ -1,4 +1,6 @@
 using System;
+using Fenrir.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +8,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Fenrir.Infrastructure.Migrations;
 
 /// <inheritdoc />
+[DbContext(typeof(FenrirDbContext))]
+[Migration("20260501141000_AddSiemIngestionTables")]
 public partial class AddSiemIngestionTables : Migration
 {
     /// <inheritdoc />
